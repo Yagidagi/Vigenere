7ef42973ac95254b5e026b3938049e6cbf759142
James Chen <jchen111@stuy.edu>
2020-10-15 15:32:32 -0400

    more progress

--- Coder.java
+++ Coder.java
@@ -4,15 +4,17 @@ public class Coder
 {
 	public static void main(String args[])
 	{
-		String str = "abcdjfdjabc";
+		String str = "DVYCNZVJIHNVMNBDFTRRSNNXPIRMFMZZOYUFXGBEHKEVDDFVMTURWDAXMDGKMZBIOJZFOZLZOHLGVMFVBIQEPOUZOBCRSOVTVGNIUJVEUZEVTOZVPIFYPMRZUCBLHCGZXJHCENNZMVOFVONCJOGCFVAUTZRKIZJRUZEPQVEKPAGYFRBIMYVKJNNNBTVYBQRFGYEZWDAXPASKIZFGMZREBIQIFBHCBOVEHOUVDDETVGNKJJANIZAVWZEZGDAUNTFVMATIPRVEHBEZNVOFVOGYFHBLUCJYFIRMFMVKJNNUBHCUSDMQMTAFWZZSFMVENTFFVGJYFIRMFMVWJIQDZNRCGDAMPGHEUVEZMTCRVNVEHWRWPMRTPASZORNIFCBLTZFROYOIJITZOBHGUCRIFVEFGZIVSTSLOZERMDZVFONEEZFGFXVRMGLNIZAVWZEDZCLGPNTVUNHTIVALQKRIIVAUPAZVUCNKJOEVRPVIFNNJUMBEHHBIBGCIJIPZQGRKPKEVWZAKNZSIPHQVMDOVSVGVMTFKFKCZOBVEUJGYFNGIFZGROYZVUCBUJXNCMTXEPXXZOBCVPKYVTCNKTJSWUCREJVPTPPAKJOUZHCGZNZGFHZGKPNRRBNFFPINJJXNEUCVJJNZPTPOJUDGLUZSFSKVJUJYROYORMGJZUCNGIDYFTJCYJXNCGGBLSDFYDVGFUCEFXNUZNNRCGPCFOCVJTRBIEDDLJZGCZONBFOBKIZFYJKGYFMRZTIBKIDAXTPEGSDFZOBVEUCVJJAGYFTOLUFAVXDGRMHBJUVYCNZAZOOUVJMQVHMRVTJZVUDZVPMBKIZETIZEZTCIVSTAVBMYPUCRJBHRWFZYZOBFKPRNIENGYFJPVBIJZUCZVUCRIFIBNJNLFVMVETPYRSXVKZJSKIZZROCNKUJRJCZYKFYEFVIQSZRURSQRJBNVEEDNEJNYVTWLTPMNCSZRWTXBDNZETFNHISJHEENVKXDGYIZEJVMSIJBUKBIQCFAGKIZFKSZRKTONBFTBLXVGVSRNIEDGJFSGIFHRUPRAKPRAZTOUVCVGKFMLNIZEVUCNKOJOCFHBCFDFNBNUVEWLNBQRJBIQTPJYVEWLSSZRQFNJYJXURGZJYPPEJQMRMJJHJXZEVPPGFGNVXIOBWMVAUMJBBBOGYFXEFXYFFGRNKFMTRAZEJUCRIF";
 
 		str.toLowerCase();
 
 		ArrayList arr = findRepeats(str);
-		for(int i=0;i<arr.size();i++)
-		{
-			System.out.println(arr.get(i));
-		}
+
+		System.out.println(findGCD(arr));
+
+		String b = encode("abcdefg", "ab");
+
+		System.out.println(b);
 	}
 
 	public static int stringToInt (String str)
@@ -30,21 +32,107 @@ public class Coder
 		return intToString(stringToInt(a)+stringToInt(b));
 	}
 
+	public static String processLine(String a, String b)
+	{
+		String result = "";
+
+		for (int i=0; i<a.length() && i<b.length(); i++)
+		{
+			result += processLetter(a.substring(i,i+1), b.substring(i,i+1));
+		}
+
+		return result;
+	}
+
+	public static String generateKey(String target, String key)
+	{
+		int reps = target.length()/key.length();
+		String result="";
+
+		for(int x=0;x<=reps;x++)
+		{
+			result+=key;
+		}
+		return result;
+	}
+
+	public static String encode(String target, String key)
+	{
+		String temp = generateKey(target, key);
+
+		return processLine(target, temp);
+	}
+
 	public static ArrayList findRepeats(String str)
 	{
 		ArrayList result = new ArrayList();
+		boolean tracker;
 		for (int i=0; i<str.length()-3; i++)
 		{
+			tracker=true;
 			String temp = str.substring(i,i+4);
 			for (int j=i+3; j<str.length()-3;j++)
 			{
 				String comp = str.substring(j,j+4);
-				if (temp.equals(comp))
+				if (temp.equals(comp) && tracker)
 				{
 					result.add(j-i);
+					tracker = false;
 				}
 			}
 		}
 		return result;
 	}
+
+	public static int findGCD (int a, int b)
+	{
+		int temp;
+		if (a < b)
+		{
+			temp = b;
+			b = a;
+			a = temp;
+		}
+		while (b!=0)
+		{
+			a = a % b;
+
+			temp = b;
+			b = a;
+			a = temp;
+		}
+		
+		return a;
+	}
+
+	public static int findGCD (ArrayList arr)
+	{
+		int result = (int) arr.get(0);
+
+		for (int i=0;i<arr.size();i++)
+		{
+			result = findGCD(result, (int) arr.get(i));
+		}
+
+		return result;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
